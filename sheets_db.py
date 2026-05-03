d79ccf8af5d66493b8e86f3764399f7221594ab3
Lacey <laceymut21@gmail.com>
2026-05-03 10:17:29 +0300

    update

--- sheets_db.py
+++ sheets_db.py
@@ -7,7 +7,7 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 SHEET_ID = "1gXfI_jZ7YAbaVz691HJrXkFKkILZ2nBeireP3dAWxg0"
 
@@ -109,7 +109,7 @@ def save_attempt(
         accurate_percentile if accurate_percentile is not None else "",
         rough_percentile if rough_percentile is not None else "",
         str(clean_email == ""),   # IsAnonymous
-        datetime.now().isoformat(timespec="seconds"),
+        datetime.now(tz=timezone(timedelta(hours=3))).isoformat(timespec="seconds"),
         str(brevo_message_id),
         str(brevo_uuid),
         str(brevo_tags),
